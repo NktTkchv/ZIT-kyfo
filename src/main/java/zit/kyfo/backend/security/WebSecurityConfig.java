e41ffe859de09770988845e333985821b60864d2
ZQR0 <yarikrech@gmail.com>
2026-07-21 12:47:11 +0300

    Вернул security

--- src/main/java/zit/kyfo/backend/security/WebSecurityConfig.java
+++ src/main/java/zit/kyfo/backend/security/WebSecurityConfig.java
@@ -21,6 +21,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 
 @Slf4j
 @EnableWebSecurity
@@ -82,5 +84,10 @@ public class WebSecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityContextRepository httpSessionSecurityContextRepository() {
+        return new HttpSessionSecurityContextRepository();
     }
 }
